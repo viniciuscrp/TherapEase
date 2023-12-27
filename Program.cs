0285eb61c7bd2f0b3dd558a7568120891e72d83a
Vinicius da Cruz <vinicius@brainov.com>
2023-12-26 23:46:09 -0300

    added HttpContextAccessor service

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddDbContext<ApiContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
