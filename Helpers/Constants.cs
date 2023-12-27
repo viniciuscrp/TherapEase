f8fe6c9231edb8c76e7793c8a811ee3cfcfbb1e5
Vinicius da Cruz <vinicius@brainov.com>
2023-12-27 01:17:59 -0300

    added Constants helper class and Cookies enum

---
+++ Helpers/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace TherapEase.Helpers
+{
+    public class Constants
+    {
+        public enum Cookies
+        {
+            [Description("X-Access-Token")]
+            AccessToken,
+            [Description("X-Refresh-Token")]
+            RefreshToken,
+            [Description("X-User-Id")]
+            UserId
+        }
+    }
+}
