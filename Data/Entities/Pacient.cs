57d28a8eea8c0e4eff0584a50321df0a6e5c89f3
Vinicius da Cruz <vinicius@brainov.com>
2024-01-14 13:37:49 -0300

    added email to pacient

--- Data/Entities/Pacient.cs
+++ Data/Entities/Pacient.cs
@@ -1,12 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
 using TherapEase.Data.Entities.Base;
 
 namespace TherapEase.Data.Entities
 {
+
+    [Index(nameof(Email), IsUnique = true)]
     public class Pacient : BaseEntity
     {
         [Column(TypeName = "varchar(100)")]
         public string Name { get; set; }
+
+        [Column(TypeName = "varchar(100)")]
+        public string Email { get; set; }
 
         public int TherapistId { get; set; }
 
---
+++ Migrations/20240114163521_AddedEmailToPacient.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TherapEase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedEmailToPacient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Pacients",
+                type: "varchar(100)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pacients_Email",
+                table: "Pacients",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Pacients_Email",
+                table: "Pacients");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Pacients");
+        }
+    }
+}
