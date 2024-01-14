0bd9fbd80dfd63ffc8ced874e0669bd94a6ff25b
Vinicius da Cruz <vinicius@brainov.com>
2024-01-14 14:05:48 -0300

    added RegisterViewModel

---
+++ Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TherapEase.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
